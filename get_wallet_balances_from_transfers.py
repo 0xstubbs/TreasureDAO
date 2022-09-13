746262229530dea8916158471e55e72f89c508ae
stubbs <stubbs0x@protonmail.com>
2022-09-13 04:30:26 -0600

    rewrote the parsing and collecting of transfers

---
+++ get_wallet_balances_from_transfers.py
@@ -0,0 +1,273 @@
+import pandas as pd
+import numpy as np
+from datetime import date
+
+# #Use the most recent df_transfers file 
+# def load_transfers_csv():
+#     print('Loading csv...')
+#     df_transfers = pd.read_csv('/home/stubbs/Documents/skycatcher/TreasureDAO/df_transfers.csv' 
+#                           , usecols=['hash', 'from', 'to', 'value', 'rawContract.address', 'metadata.blockTimestamp']
+#                             , parse_dates=['metadata.blockTimestamp']
+#                           )
+#     return df_transfers
+# def get_transformed_transfers(_df_transfers_full):
+#     print('Transforming df...')
+#     df_transfers_in = _df_transfers_full[['hash', 'to', 'value', 'metadata.blockTimestamp']].copy()
+#     df_transfers_in.rename(columns = {'hash': 'tx_hash', 'to':'address', 'value':'amount', 'metadata.blockTimestamp':'tx_timestamp'}, inplace=True)
+    
+#     df_transfers_out = _df_transfers_full[['hash', 'from', 'value', 'metadata.blockTimestamp']].copy()
+#     df_transfers_out['value'] = df_transfers_out['value'].apply(lambda x: -1.0* x)
+#     df_transfers_out.rename(columns = {'hash': 'tx_hash', 'from':'address', 'value':'amount', 'metadata.blockTimestamp':'tx_timestamp'}, inplace=True)
+    
+#     transfer_in_n_out=pd.concat([df_transfers_in, df_transfers_out], axis=0)
+#     transfer_in_n_out['date'] = pd.to_datetime(transfer_in_n_out['tx_timestamp'].apply(lambda x:x.date()))
+    
+#     return transfer_in_n_out
+
+# def get_all_unique_addresses(_df_transfers_full):
+#     print('Getting all unique addresses...')
+#     wallets = pd.DataFrame(_df_transfers_full['address'].unique())
+#     wallets['key']=1
+#     return wallets
+
+# def get_df_all_wallets_all_days(_transformed_transfers):
+    
+#     unique_wallets = pd.DataFrame(_transformed_transfers['address'].unique())
+#     unique_wallets['key']=1
+    
+#     start_day = pd.to_datetime(_transformed_transfers['tx_timestamp'].dt.date).min()
+#     end_day = pd.to_datetime(_transformed_transfers['tx_timestamp'].dt.date).max()
+#     date_range = pd.DataFrame(pd.date_range(start_day, end_day, inclusive="both"))
+#     date_range['key']=1
+    
+#     wallets_all_days=pd.merge(unique_wallets, date_range, how='outer', on='key').drop(columns=['key']).rename(columns={'0_x':'wallet_address', '0_y':'date'})
+#     return wallets_all_days
+
+# def get_df_all_wallets_all_days_n_changes(_wallets_all_days, _transformed_transfers):
+#     wallet_all_days_n_changes = _wallets_all_days[['date', 'wallet_address']].merge(_transformed_transfers, how='left', 
+#                                                                                     left_on=['wallet_address', 'date'], 
+#                                                                                     right_on=['address', 'date']).drop(columns=['address']).fillna(0)
+#     wallet_all_days_n_changes.sort_values('date', ascending=True, inplace=True)
+#     wallet_all_days_n_changes['cumsum'] = wallet_all_days_n_changes.groupby(['wallet_address'])['amount'].cumsum()
+#     wallet_all_days_n_changes = wallet_all_days_n_changes[['date', 'wallet_address', 'amount', 'cumsum']]
+#     wallet_all_days_n_changes = wallet_all_days_n_changes[wallet_all_days_n_changes['cumsum'] > 1]
+    
+#     return wallet_all_days_n_changes
+# def get_unique_holders_per_day(_wallet_all_days_n_changes):
+#     return _wallet_all_days_n_changes.groupby('date')['wallet_address'].count()
+
+# def get_wallet_bal_net_change(_wallet_balances_n_changes):
+#     return _wallet_balances_n_changes[(_wallet_balances_n_changes['wallet_address'] != '0x0000000000000000000000000000000000000000') & (_wallet_balances_n_changes['cumsum'] > 1)]
+
+# def mints_n_burns(_df_transfers, _type):
+#     mints_n_burns = _df_transfers[_df_transfers['wallet_address'] == '0x0000000000000000000000000000000000000000']
+#     mints_burns_grp = mints_n_burns[['date', 'amount']]
+#     mints_burns_grp = mints_burns_grp.groupby('date').agg({'amount':'sum'})
+#     mints_burns_grp['cumsum'] = mints_burns_grp['amount'].cumsum()
+    
+#     if _type == 'df':
+#         return mints_burns_grp
+#     elif _type == 'total_supply':
+#         return abs(mints_burns_grp['cumsum'].min())
+ 
+
+# df_transfers_full = pd.read_csv('/home/stubbs/Documents/skycatcher/TreasureDAO/df_transfers.csv', usecols=['hash', 'from', 'to', 'value', 'rawContract.address', 'metadata.blockTimestamp'], parse_dates['metadata.blockTimestamp'])
+# print('Transforming df...')
+# df_transfers_in = _df_transfers_full[['hash', 'to', 'value', 'metadata.blockTimestamp']].copy()
+# df_transfers_in.rename(columns = {'hash': 'tx_hash', 'to':'address', 'value':'amount', 'metadata.blockTimestamp':'tx_timestamp'}, inplace=True)
+    
+# df_transfers_out = _df_transfers_full[['hash', 'from', 'value', 'metadata.blockTimestamp']].copy()
+# # df_transfers_out['value'] = df_transfers_out['value'].apply(lambda x: -1.0* x)
+# df_transfers_out['value'] = df_transfers_out['value']*-1.0
+
+# df_transfers_out.rename(columns = {'hash': 'tx_hash', 'from':'address', 'value':'amount', 'metadata.blockTimestamp':'tx_timestamp'}, inplace=True)
+    
+# transfer_in_n_out=pd.concat([df_transfers_in, df_transfers_out], axis=0)
+# transfer_in_n_out['date'] = pd.to_datetime(transfer_in_n_out['tx_timestamp'])##.apply(lambda x:x.date()))
+
+# df_transfers = transfer_in_n_out
+
+
+
+# # df_transfers_full = load_transfers_csv()
+# # df_transfers = get_transformed_transfers(df_transfers_full)
+
+# print(df_transfers_full.info())
+# print(df_transfers_full.head())
+
+# # unique_addresses = get_all_unique_addresses(df_transfers)
+
+
+# print(unique_addresses)
+# # print(df_transfers_full.head())
+
+
+
+# df_all_wallets_all_days_n_changes = get_df_all_wallets_all_days_n_changes(get_df_all_wallets_all_days(df_transfers), df_transfers)
+
+#------------------------------------------------------------------------------#
+#--------------------Get Unique Wallets------------------------------------
+# unique_wallets = pd.DataFrame(_transformed_transfers['address'].unique())
+print('Getting all unique addresses...')
+wallets = df_transfers['address'].drop_duplicates()
+wallets['key']=1
+
+##-------------
+#-----Create a date range dataframe
+
+start_day = pd.to_datetime(df_transfers['tx_timestamp'].dt.date).min()
+end_day = pd.to_datetime(df_transfers['tx_timestamp'].dt.date).max()
+date_range = pd.DataFrame(pd.date_range(start_day, end_day, inclusive="both"))
+date_range['key']=1
+
+wallets_all_days=pd.merge(unique_wallets, date_range, how='outer', on='key').drop(columns=['key']).rename(columns={'0_x':'wallet_address', '0_y':'date'})
+
+
+
+
+print(df_all_wallets_all_days_n_changes.head())
+print(df_all_wallets_all_days_n_changes.info())
+
+unique_holders_per_day = get_unique_holders_per_day(df_all_wallets_all_days_n_changes)
+print(unique_holders_per_day.head())
+print(unique_holders_per_day.info())
+
+wallet_balance_n_changes = get_wallet_bal_net_change(df_all_wallets_all_days_n_changes)
+print(wallet_balance_n_changes.head())
+print(wallet_balance_n_changes.info())
+
+df_mints_n_burns = mints_n_burns(df_transfers, 'df')
+total_supply = mints_n_burns(df_transfers, 'total_supply')
+
+df_mints_n_burns.to_csv(f'{date.today()}_magic_supply_over_time.csv')
+wallet_balance_n_changes.to_csv(f'{date.today()}_wallet_balances.csv')
+
+
+#------------------------------------------------------------------------------------------------------------#
+#
+#                                          Read the data from CSV Files
+#
+#------------------------------------------------------------------------------------------------------------#
+
+#Read in the csv/parquet file that has the raw transaction data
+df_transfers = pd.read_csv('/home/stubbs/Documents/skycatcher/TreasureDAO/df_transfers.csv' 
+                          , usecols=['hash', 'from', 'to', 'value', 'rawContract.address', 'metadata.blockTimestamp']
+                            , parse_dates=['metadata.blockTimestamp']
+                          )
+
+#Split the raw data into two dataframes; 1. transfers out, 2. transfers in
+
+#---------------------------------------------Create a 'transfers_out' dataframe-------------------------------#
+#The values in the 'transfers_out' dataframe will be multiplied by -1.0 to show that the wallet balance is going down
+df_transfers_out = df_transfers[['hash', 'from', 'value', 'metadata.blockTimestamp']].copy()
+df_transfers_out['value']=df_transfers_out['value']*-1.0
+df_transfers_out.rename(columns = {'hash': 'tx_hash', 'from':'address', 'value':'amount', 'metadata.blockTimestamp':'tx_timestamp'}, inplace=True)
+
+#---------------------------------------------Create a 'transfers_in' dataframe-------------------------------#
+#The values in 'transfers_in' are positive
+df_transfers_in = df_transfers[['hash', 'to', 'value', 'metadata.blockTimestamp']].copy()
+df_transfers_in.rename(columns = {'hash': 'tx_hash', 'to':'address', 'value':'amount', 'metadata.blockTimestamp':'tx_timestamp'}, inplace=True)
+
+#-----------------------------Use Pandas concat to combine the 'in' and 'out' dataframes------------------------------#
+transfers_all=pd.concat([df_transfers_in, df_transfers_out], axis=0)
+
+#Create a 'date' column we will not be looking at individual txns. 
+transfers_all['date'] = pd.to_datetime(transfer_in_n_out['tx_timestamp']).dt.date
+transfers_all = transfers_all[['date', 'tx_timestamp', 'address', 'amount']].reset_index()
+
+#------------------------------------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------------------------------------#
+#Create a df that has all the wallet addresses that have interacted with $MAGTC and the date of their first interaction
+
+wallets_and_first_interaction = transfers_all.drop_duplicates(subset='address', keep='first')[['date', 'address']]
+wallets_and_first_interaction['key']=1                                                   #Add a ['key'] column that can be used to join onto
+wallets_and_first_interaction.columns = ['first_interaction_date', 'address', 'key']     #Rename the columns
+
+#------------------------------------------------------------------------------------------------------------#
+# Create a data_range df. You will join the wallet & first interactions onto the date range so we can create 
+# a df with daily wallet balances that won't have interruptions
+start_day=transfers_all['date'].min()
+end_day=transfers_all['date'].max()
+
+date_range = pd.DataFrame(pd.date_range(start_day, end_day, inclusive="both")).rename(columns={0:'date'}).reset_index()
+date_range['key']=1
+
+#------------------------------------------------------------------------------------------------------------#
+# Merge the date range on wallet_list on the 'key' column created earlier. 
+merged_wallets_date_range = date_range.merge(wallets_and_first_interaction, how='left', on='key')
+
+#Now merge all transfers onto the merged wallet_date_range df
+merged_wallets_transfers=merged_wallets_date_range.merge(transfers_all, how='left', left_on =['date', 'address'], right_on=['date', 'address']).drop(['index_x', 'key', 'index_y'], axis=1)
+
+#Create a mask to remove wallet data from the df on days that are earlier than the first wallet_interaction. This
+# will help prevent having millions of rows of zeros before the wallets have even interacted
+mask = merged_wallets_transfers['first_interaction_date']<=merged_wallets_transfers['date']
+filtered_wallet_transfer_df=merged_wallets_transfers.loc[mask]
+filtered_wallet_transfer_df.loc[:,'amount'].fillna(0)
+
+#Group the transfers by date and sum up 'amount' columns to get the net daily change in MAGIC for a wallet
+grouped_filtered_wallet_transfer=filtered_wallet_transfer_df.groupby(['date', 'address'])['amount'].sum().reset_index()
+
+#Get a running total of transfers to get daily wallet balances
+grouped_filtered_wallet_transfer['cumsum']=grouped_filtered_wallet_transfer.groupby(['address'])['amount'].cumsum()
+
+#Now remove all rows for addresses that no longer have a balance and set the dataframe index to the date
+wallet_balances=grouped_filtered_wallet_transfer[grouped_filtered_wallet_transfer['cumsum']>0].set_index('date')
+
+#-----------------------------------------------------------------------------------------------------------------
+#Save the wallet_balances to a .csv/.parquet file
+
+wallet_balances.to_parquet(f'{date.today()}_balances_by_day.parquet')
+wallet_balances.to_csv(f'{date.today()}_balances_by_day.csv')
+#-----------------------------------------------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------------------------------------------#
+#----------------------------------Upload parquet file to Amazon S3 Bucket----------------------------------------#
+import boto3
+import os
+from dotenv import load_dotenv
+
+
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+#Get Amazon S3 credentials from .env
+load_dotenv()
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')         #Access key should start w/ 'AKIA'. If creds are downloaded from S3 it will be in the 'Password' column
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') #Secret access key should start w/ 'UEVCA'. Downloaded creds have it in the 'Access key ID' column.
+
+s3 = boto3.client('s3',
+                   region_name='us-west-1',
+                   aws_access_key_id=AWS_ACCESS_KEY_ID,
+                   aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+bucket_name = "stubbs-file-storage-streamlit"
+object_name = "balances_by_day.parquet"
+
+# s3=boto3.client('s3')
+with open('./2022-09-12_balances_by_day.parquet', "rb") as f:
+    s3.upload_fileobj(f, bucket_name, object_name)
+    
+#----------------------------------------------------------------------------------------------------------------------#
